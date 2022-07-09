b6b0dd3632793e47662981440e2f799241cf8462
Debo <deborahkollman@gmail.com>
2022-07-09 17:45:27 -0300

    force false

--- api/index.js
+++ api/index.js
@@ -5,9 +5,8 @@ const server = require('./server');
 
 const { PORT } = process.env || 3000;
 
-connection.sync({ force: true }).then(() => {
+connection.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Servidor levantado en el puerto: ${PORT}`);
   });
 });
- 
